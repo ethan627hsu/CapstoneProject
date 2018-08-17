d94670eaa6ba9e73e5c25d19213c55fa93079054
Ethan Hsu <ethan627hsu@gmail.com>
2018-08-16 22:32:11 -0700

    Added main UI activity and fragment

---
+++ app/src/main/java/com/ethan627hsu/wake/wake/MainUIFragment.java
@@ -0,0 +1,33 @@
+package com.ethan627hsu.wake.wake;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainUIFragment extends Fragment {
+
+    public MainUIFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_ui, container, false);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}

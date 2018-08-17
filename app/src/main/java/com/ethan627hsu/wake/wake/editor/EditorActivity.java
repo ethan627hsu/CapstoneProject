d3dd624d62a197fb118a78419033a276e62e685e
Ethan Hsu <ethan627hsu@gmail.com>
2018-08-16 22:58:11 -0700

    Added editor activities and fragments

---
+++ app/src/main/java/com/ethan627hsu/wake/wake/editor/EditorActivity.java
@@ -0,0 +1,15 @@
+package com.ethan627hsu.wake.wake.editor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ethan627hsu.wake.wake.R;
+
+public class EditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+    }
+}
